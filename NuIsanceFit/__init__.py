dcfca648326ea3c86d7ac465840cf40e238bf47f
Boggle-Head <b.smithers.actual@gmail.com>
2021-05-05 18:09:22 -0500

    added optional resoures parameter. this is set on import

--- NuIsanceFit/__init__.py
+++ NuIsanceFit/__init__.py
@@ -6,3 +6,6 @@ from .data import Data
 f = open(os.path.join(os.path.dirname(__file__),"resources/steering.json"),'r')
 steering = json.load(f)
 f.close()
+
+if steering["resources"]["resource_dir"]=="":
+    steering["resources"]["resource_dir"] = os.path.join(os.path.dirname(__file__),"resources")
