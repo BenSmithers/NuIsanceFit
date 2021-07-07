d805c18d8c9fae5deabe5d7ef22dd3e08f55bce5
Boggle-Head <b.smithers.actual@gmail.com>
2021-07-07 14:01:15 -0500

    dealing with this stuff..

--- NuIsanceFit/fit.py
+++ NuIsanceFit/fit.py
@@ -7,6 +7,10 @@ from NuIsanceFit import Logger
 from NuIsanceFit.nuthread import ThreadManager
 
 import numpy as np
+from time import time
+
+from multiprocessing import Pool as ThreadPool
+from multiprocessing.dummy import Pool as ThreadPool
 
 class Fitter:
     def __init__(self, steering):
@@ -29,26 +33,29 @@ class Fitter:
         result = np.zeros(shape=shape)
 
         dims = tuple([len(sim.edges[i])-1 for i in range(len(sim.edges))])
-        
-        #def getter
-
 
         def wrapper(dataobj):
-            result = [0 for i in range(dims[1])]
-            for j in range(dims[1]):
+            result = [0 for j in range(len(dataobj))]
+            for j in range(len(dataobj)):
                 for event in dataobj[j][0][0][0]:
                     result[j] += self._llhobj._simWeighter(event)
             return result
 
-        
-        for i in range(dims[0]):
-            for j in range(dims[1]):
-                # let's just fix the other dimensions for now! 
-                for event in sim[i][j][0][0][0]:
-                    result[i][j]+=self._llhobj._simWeighter(event)
-        
+        #for i in range(dims[0]):
+        #    for j in range(dims[1]):
+        #        # let's just fix the other dimensions for now! 
+        #        for event in sim[i][j][0][0][0]:
+        #            result[i][j]+=self._llhobj._simWeighter(event)
 
-        #result = ThreadManager(wrapper, sim.fill)
+        #stepsize = int(dims[0]/4)
+        #subsets = [sim.fill[stepsize*i:stepsize*(i+1)] for i in range(4)]
+        #subsets[3] = sim.fill[3*stepsize:]
+
+        pool = ThreadPool(1)
+        result = pool.map(wrapper, sim)        
+
+
+        #result = ThreadManager(wrapper, subsets)
 
 
         return result
